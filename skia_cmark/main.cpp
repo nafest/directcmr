f4cfee83e5a695046f93a71717c7ad1e620ba36b
Stefan Winkler <mail@stefanwinkler.de>
2017-03-06 08:46:47 +0100

    Add skia font handling

--- skia_cmark/main.cpp
+++ skia_cmark/main.cpp
@@ -1,6 +1,13 @@
+#include "document.h"
+
 #include "skia_renderer.h"
 
-int main(int argc, const char* argv[])
-{
+int main(int argc, const char *argv[]) {
+    skia_renderer rndr;
+    document d = document::fromString("Hello *world*");
+    d.set_renderer(&rndr);
+    d.layout(100);
+    d.render();
+
     return 0;
 }
--- skia_cmark/skia_renderer.cpp
+++ skia_cmark/skia_renderer.cpp
@@ -2,28 +2,46 @@
 
 #include "SkTypeface.h"
 
-class skia_font : public font {};
+class skia_font : public font {
+  public:
+    skia_font(const std::string &family, const std::string &style, int size) {
+        sk_sp<SkFontMgr> fmgr = SkFontMgr::RefDefault();
+
+        int weight = SkFontStyle::kNormal_Weight;
+        if (style == "Bold" || style == "BoldItalic")
+            weight = SkFontStyle::kBold_Weight;
+
+        SkFontStyle font_style{weight, SkFontStyle::kNormal_Width,
+                               SkFontStyle::kUpright_Slant};
+
+        m_typeface.reset(fmgr->matchFamilyStyle(family.c_str(), font_style));
+
+        m_paint.setTypeface(m_typeface);
+        m_paint.setTextSize(size);
+        m_paint.setAntiAlias(true);
+    }
+
+    extents string_extents(const std::string &text) const {
+        SkRect bounds;
+        m_paint.measureText(text.c_str(), text.length(), &bounds);
+        return extents(bounds.width(), bounds.height());
+    }
+
+  private:
+    sk_sp<SkTypeface> m_typeface;
+    SkPaint m_paint;
+};
 
 font *skia_renderer::create_font(const std::string &family,
                                  const std::string &style, int size) {
-    sk_sp<SkFontMgr> fmgr = SkFontMgr::RefDefault();
-
-    int weight = SkFontStyle::kNormal_Weight;
-    if (style == "Bold" || style == "BoldItalic")
-        weight = SkFontStyle::kBold_Weight;
-
-    SkFontStyle font_style{weight, SkFontStyle::kNormal_Width,
-                           SkFontStyle::kUpright_Slant};
-
-    SkTypeface *typeface = fmgr->matchFamilyStyle(family.c_str(), font_style);
-
-    return nullptr;
+    return new skia_font(family, style, size);
 }
 
 extents skia_renderer::string_extents(const font *fnt,
                                       const std::string &string) {
     extents ext;
-    return ext;
+    const skia_font *sfnt = static_cast<const skia_font *>(fnt);
+    return sfnt->string_extents(string);
 }
 
 void skia_renderer::draw_string(const std::string &text, const position &pos,
